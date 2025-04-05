65b0f7b529889ef826bf11ce7a2eb74ba359eb6e
andreea-belu25 <andreeabelu254@gmail.com>
2025-04-05 14:26:13 +0300

    webserver_log.py

--- app/__init__.py
+++ app/__init__.py
@@ -15,7 +15,7 @@ webserver = Flask(__name__)
 # webserver.task_runner.start()
 
 webserver.data_ingestor = DataIngestor("./nutrition_activity_obesity_usa_subset.csv")
-webserver.tasks_runner = ThreadPool(webserver.data_ingestor) 
+webserver.tasks_runner = ThreadPool(webserver.data_ingestor)
 webserver.job_counter = 1
 
 from app import routes
