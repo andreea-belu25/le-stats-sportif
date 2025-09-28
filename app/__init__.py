d8f99a933c2b856e2e132c068966d93be33aa4ec
Andreea-Daniela Belu <119062819+andreea-belu25@users.noreply.github.com>
2025-09-28 23:08:31 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -12,8 +12,6 @@ if not os.path.exists('results'):
 
 webserver = Flask(__name__)
 
-# webserver.task_runner.start()
-
 webserver.data_ingestor = DataIngestor("./nutrition_activity_obesity_usa_subset.csv")
 webserver.tasks_runner = ThreadPool(webserver.data_ingestor) 
 webserver.job_counter = 1
